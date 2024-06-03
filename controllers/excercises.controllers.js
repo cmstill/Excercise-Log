e1853562adae65e90fc0c7ce999486b92717ee83
cmstill <cameron.still@gmail.com>
2024-06-02 22:16:17 -0500

    added put, delete, and patch for api/v1/excercise<id>

--- controllers/excercises.controllers.js
+++ controllers/excercises.controllers.js
@@ -1,16 +1,31 @@
 export const getExcercises = async (req, res, next) => {
-	console.log('Controller: getExcercises');
-	res.status(200).json([]);
+  console.log('Controller: getExcercises');
+  res.status(200).json([]);
 };
 
 export const createExcercise = async (req, res, next) => {
-	console.log('Controller: createExcercise');
-	res.status(200).json({});
+  console.log('Controller: createExcercise');
+  res.status(200).json({});
 };
 
 export const getExcercise = async (req, res, next) => {
-	console.log(`Controller: getExcercise(${req.params.id})`);
-	res.status(200).json({});
+  console.log(`Controller: getExcercise(${req.params.id})`);
+  res.status(200).json({});
+};
+
+export const replaceExcercise = async (req, res, next) => {
+  console.log(`Controller: replaceExcercise(${req.params.id})`);
+  res.status(200).json({});
+};
+
+export const deleteExcercise = async (req, res, next) => {
+  console.log(`Controller: deleteExcercise(${req.params.id})`);
+  res.status(200).json({});
+};
+
+export const updateExcercise = async (req, res, next) => {
+  console.log(`Controller: updateExcercise(${req.params.id})`);
+  res.status(200).json({});
 };
 // every resource I am supporting will need a function in the controller
 /* ie if I am doing a get HTTP method for excercises, I'll make a getExcercises function and etc. for delete, update etc */
--- routes/excercises.routes.js
+++ routes/excercises.routes.js
@@ -1,16 +1,25 @@
 import express from 'express';
-import { getExcercises, createExcercise, getExcercise } from '../controllers/excercises.controllers.js';
+import { getExcercises, createExcercise, getExcercise, replaceExcercise, deleteExcercise, updateExcercise } from '../controllers/excercises.controllers.js';
 
 const excercisesRouter = express.Router(); // Router is a method on express object that we're importing above...it's what allows us to set up routes...it is a method that accepts routes and then the function you wand to call on those routes...and these funcitons are found in the controller
 
 // GET to /api/v1/excercises
 excercisesRouter.get('/', getExcercises);
 
-//POST /api/v1/excercises
+// POST /api/v1/excercises
 excercisesRouter.post('/', createExcercise);
 
-//GET /api/v1/excercises/<id>
+// GET /api/v1/excercises/<id>
 excercisesRouter.get('/:id', getExcercise); // this is a get to a single excercise by a user provided id...id's populate in req.params(we write this in our controller)
+
+// PUT /api/v1/excercises/<id>
+excercisesRouter.put('/:id', replaceExcercise);
+
+//DELETE /api/v1/excercises/<id>
+excercisesRouter.delete('/:id', deleteExcercise);
+
+// PATCH /api/v1/excercises/<id>
+excercisesRouter.patch('/:id', updateExcercise);
 
 export default excercisesRouter;
 
